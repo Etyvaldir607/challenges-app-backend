2a4ae01eacfc4fe9cb85fd4e4a03d2dd9a027439
Erick Machicado <etyvaldirmc@gmail.com>
2024-10-04 23:03:26 -0400

    added - config api response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,26 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function sendResponse($result, $message, $code = 200) {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'code' => 200
+        ];
+        return response()->json($response, $code);
+    }
+
+    public function sendError($error, $errorMessages, $code = 200) {
+        $response = [
+            'success' => false,
+            'data' => $error,
+        ];
+
+        if ( !empty($errorMessages)) {
+            $response['info'] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
+
 }
